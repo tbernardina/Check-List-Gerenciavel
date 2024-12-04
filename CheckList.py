d1d73652f3ec1498a955b00c5ac3b66058441c6d
Thiago Reis Dalla Bernardina <trdallabernardina@gmail.com>
2024-12-04 17:06:46 -0300

    Atualização nova
    Refiz o código para ficar mais profissional e organizado com menos redundância e adicionei funções novas.

--- CheckList.py
+++ CheckList.py
@@ -1,128 +1,187 @@
 import tkinter as tk
 from tkinter import messagebox
-import pymysql
 from Conexao import conexao_db
 import FuncoesBanco as FB
 
-# Funções da interface
-def verificar_login():
-    global nivel_acesso
-    usuario = entrada_usuario.get()
-    senha = entrada_senha.get()
+# Configurações gerais de estilo
+ESTILO_JANELA = {"bg": "#FFF8C6"}
+ESTILO_TEXTO = {"font": ("Arial", 12), "bg": "#FFF8C6", "fg": "#6D4C41"}
+ESTILO_TITULO = {"font": ("Comic Sans MS", 18, "bold"), "bg": "#FFF8C6", "fg": "#6D4C41"}
+ESTILO_TEXTO_LOGIN = {"font": ("Arial", 12)}
+ESTILO_LISTA_TAREFAS = {"font": ("Arial", 12), "fg": "#6D4C41"}
 
-    nivel = FB.verificar_credenciais(usuario, senha)
-    if nivel:
-        global nivel_acesso
-        nivel_acesso = nivel
-        janela_login.destroy()
-        abrir_interface_principal()
-    else:
-        messagebox.showerror("Erro de Login", "Usuário ou senha incorretos.")
+class App:
+    def __init__(self):
+        self.usuario = None
+        self.nivel_acesso = None
+        self.setor_exec = None
 
-def adicionar_tarefa():
-    if nivel_acesso != "1":
-        messagebox.showwarning("Permissão Negada", "Apenas supervisores podem adicionar tarefas.")
-        return
+        self.iniciar_login()
+    
+    #TELA PARA VERIFICAR O NIVEL DE ACESSO E O SETOR DO USUARIO
+    def iniciar_login(self):
+        self.janela_login = tk.Tk()
+        self.janela_login.title("Login")
 
-    tarefa = entrada_tarefa.get()
-    if tarefa:
-        FB.adicionar_tarefa_db(tarefa)
-        carregar_lista_tarefas()
-        entrada_tarefa.delete(0, tk.END)
-    else:
-        messagebox.showwarning("Aviso", "Digite uma tarefa antes de adicionar.")
+        frame_login = tk.Frame(self.janela_login, padx=20, pady=20)
+        frame_login.pack()
 
-def remover_tarefa():
-    if nivel_acesso != "1":
-        messagebox.showwarning("Permissão Negada", "Apenas supervisores podem remover tarefas.")
-        return
+        tk.Label(frame_login, text="Usuário:", **ESTILO_TEXTO_LOGIN).grid(row=0, column=0, pady=5)
+        self.entrada_usuario = tk.Entry(frame_login)
+        self.entrada_usuario.grid(row=0, column=1, pady=5)
 
-    try:
-        index_selecionado = lista_tarefas.curselection()[0]
-        id_tarefa = lista_tarefas.get(index_selecionado).split(" - ")[0]
-        FB.remover_tarefa_bd(id_tarefa)
-        carregar_lista_tarefas()
-    except IndexError:
-        messagebox.showwarning("Aviso", "Selecione uma tarefa para remover.")
+        tk.Label(frame_login, text="Senha:", **ESTILO_TEXTO_LOGIN).grid(row=1, column=0, pady=5)
+        self.entrada_senha = tk.Entry(frame_login, show="*")
+        self.entrada_senha.grid(row=1, column=1, pady=5)
 
-def marcar_concluida():
-    try:
-        index_selecionado = lista_tarefas.curselection()[0]
-        id_tarefa = lista_tarefas.get(index_selecionado).split(" - ")[0]
-        FB.atualizar_status_tarefa(id_tarefa, "Concluída")
-        carregar_lista_tarefas()
-    except IndexError:
-        messagebox.showwarning("Aviso", "Selecione uma tarefa para marcar como concluída.")
+        botao_login = tk.Button(frame_login, text="Login", command=self.verificar_login)
+        botao_login.grid(row=2, columnspan=2, pady=10)
 
-def carregar_lista_tarefas():
-    lista_tarefas.delete(0, tk.END)
-    tarefas = FB.carregar_tarefas()
-    for tarefas in tarefas:
-        lista_tarefas.insert(tk.END, f"{tarefas['TAREFAS_ID']} - {tarefas['DESCRICAO']} ({tarefas['STATUS']})")
+        conexao_db()
+        self.janela_login.mainloop()
 
-# Interface Principal
-def abrir_interface_principal():
-    janela_principal = tk.Tk()
-    janela_principal.title("Gerenciador de Checklists")
+    def verificar_login(self):
+        usuario = self.entrada_usuario.get()
+        senha = self.entrada_senha.get()
 
-    # Entrada de nova tarefa
-    frame_entrada = tk.Frame(janela_principal)
-    frame_entrada.pack(pady=10)
+        nivel = FB.verificar_credenciais(usuario, senha)
+        setor = FB.verificar_setor(usuario, senha)
 
-    global entrada_tarefa
-    entrada_tarefa = tk.Entry(frame_entrada, width=40)
-    entrada_tarefa.pack(side=tk.LEFT, padx=5)
+        if nivel:
+            self.usuario = usuario
+            self.nivel_acesso = nivel
+            self.setor_exec = setor
+            self.janela_login.destroy()
+            self.abrir_interface_principal()
+        else:
+            messagebox.showerror("Erro de Login", "Usuário ou senha incorretos.")
+    def detalhes_tarefa(self, event):
+        try:
+            # Recuperar índice da tarefa selecionada
+            index_selecionado = self.lista_tarefas.curselection()[0]
+            tarefa_selecionada = self.lista_tarefas.get(index_selecionado)
+            id_tarefa = tarefa_selecionada.split(" - ")[0].strip(" ● ").strip()
+            detalhes = FB.select_tarefas(id_tarefa)
+            popup, popup_conteudo = self.criar_popup(f"DETALHES DA TAREFA {id_tarefa}", lambda: popup.destroy())
 
-    botao_adicionar = tk.Button(frame_entrada, text="Adicionar", command=adicionar_tarefa)
-    botao_adicionar.pack(side=tk.LEFT)
+            tk.Label(popup_conteudo, text=)
+    #INTERFACE PRINC
+    def abrir_interface_principal(self):
+        self.janela_principal = tk.Tk()
+        self.janela_principal.title("Gerenciador de Checklists")
+        self.janela_principal.geometry("400x450")
+        self.janela_principal.configure(**ESTILO_JANELA)
 
-    # Lista de tarefas
-    frame_lista = tk.Frame(janela_principal)
-    frame_lista.pack(pady=10)
+        frame_titulo = tk.Frame(self.janela_principal, **ESTILO_JANELA)
+        frame_titulo.pack(pady=10)
+        tk.Label(frame_titulo, text="Bloco de Anotações", **ESTILO_TITULO).pack()
 
-    global lista_tarefas
-    lista_tarefas = tk.Listbox(frame_lista, width=50, height=15)
-    lista_tarefas.pack(side=tk.LEFT)
+        frame_lista = tk.Frame(self.janela_principal, **ESTILO_JANELA)
+        frame_lista.pack(pady=20)
 
-    scrollbar = tk.Scrollbar(frame_lista, orient=tk.VERTICAL, command=lista_tarefas.yview)
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-    lista_tarefas.config(yscrollcommand=scrollbar.set)
+        self.lista_tarefas = tk.Listbox(frame_lista, width=50, height=15, **ESTILO_LISTA_TAREFAS)
+        self.lista_tarefas.pack(side=tk.LEFT, padx=5)
+        self.lista_tarefas.bind("<Double-Button-1>", sel.detalhes_tarefa)
 
-    # Botões de controle
-    frame_botoes = tk.Frame(janela_principal)
-    frame_botoes.pack(pady=10)
+        scrollbar = tk.Scrollbar(frame_lista, orient=tk.VERTICAL, command=self.lista_tarefas.yview)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.lista_tarefas.config(yscrollcommand=scrollbar.set)
 
-    botao_remover = tk.Button(frame_botoes, text="Remover", command=remover_tarefa)
-    botao_remover.pack(side=tk.LEFT, padx=5)
+        frame_botoes = tk.Frame(self.janela_principal, **ESTILO_JANELA)
+        frame_botoes.pack(pady=10)
 
-    botao_concluida = tk.Button(frame_botoes, text="Marcar como Concluída", command=marcar_concluida)
-    botao_concluida.pack(side=tk.LEFT, padx=5)
+        botao_remover = tk.Button(frame_botoes, text="Remover", command=self.remover_tarefa)
+        botao_remover.pack(side=tk.LEFT, padx=5)
 
-    # Configurar permissões
-    if nivel_acesso == "0":
-        botao_adicionar.config(state=tk.DISABLED)
-        botao_remover.config(state=tk.DISABLED)
+        botao_concluida = tk.Button(frame_botoes, text="Marcar como Concluída", command=self.marcar_concluida)
+        botao_concluida.pack(side=tk.LEFT, padx=5)
 
-    carregar_lista_tarefas()
-    janela_principal.mainloop()
+        botao_adicionar = tk.Button(frame_botoes, text="Adicionar", command=self.adicionar_tarefa)
+        botao_adicionar.pack(side=tk.LEFT, padx=5)
 
-# Janela de Login
-janela_login = tk.Tk()
-janela_login.title("Login")
+        if self.nivel_acesso != "1":
+            botao_remover.config(state=tk.DISABLED)
+            botao_adicionar.config(state=tk.DISABLED)
 
-frame_login = tk.Frame(janela_login, padx=20, pady=20)
-frame_login.pack()
+        self.carregar_lista_tarefas()
+        self.janela_principal.mainloop()
 
-tk.Label(frame_login, text="Usuário:").grid(row=0, column=0, pady=5)
-entrada_usuario = tk.Entry(frame_login)
-entrada_usuario.grid(row=0, column=1, pady=5)
+    def carregar_lista_tarefas(self):
+        self.lista_tarefas.delete(0, tk.END)
+        tarefas = FB.carregar_tarefas(self.nivel_acesso, self.setor_exec)
+        for tarefa in tarefas:
+            self.lista_tarefas.insert(tk.END, f" ● {tarefa['TAREFAS_ID']} - {tarefa['DESCRICAO']} ({tarefa['STATUS']})")
 
-tk.Label(frame_login, text="Senha:").grid(row=1, column=0, pady=5)
-entrada_senha = tk.Entry(frame_login, show="*")
-entrada_senha.grid(row=1, column=1, pady=5)
+    def criar_popup(self, titulo, comando_salvar):
+        popup = tk.Toplevel()
+        popup.title(titulo)
+        popup.configure(**ESTILO_JANELA)
+        popup.transient(self.janela_principal)
+        popup.grab_set()
 
-botao_login = tk.Button(frame_login, text="Login", command=verificar_login)
-botao_login.grid(row=2, columnspan=2, pady=10)
+        popup_conteudo = tk.Frame(popup, **ESTILO_JANELA)
+        popup_conteudo.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
-conexao_db()
-janela_login.mainloop()
+        popup_salvar = tk.Frame(popup, **ESTILO_JANELA)
+        popup_salvar.pack(fill=tk.X, expand=True, padx=10, pady=10)
+
+        botao_salvar = tk.Button(popup_salvar, text="Salvar", command=comando_salvar)
+        botao_salvar.pack(pady=10)
+        return popup, popup_conteudo
+
+    def adicionar_tarefa(self):
+        def salvar_tarefa():
+            titulo = entrada_titulo.get().strip()
+            descricao = entrada_tarefa.get("1.0", tk.END).strip()
+            setor = escolha_opcoes.get()
+            if titulo and descricao and setor:
+                FB.adicionar_tarefa_db(titulo, descricao, setor)
+                self.carregar_lista_tarefas()
+                popup.destroy()
+            else:
+                messagebox.showwarning("Aviso", "Preencha todos os campos antes de salvar.")
+        popup, popup_conteudo = self.criar_popup("Adicionar Tarefa", salvar_tarefa)
+        tk.Label(popup_conteudo, text="Título da Tarefa:", **ESTILO_TEXTO).pack(pady=5)
+        entrada_titulo = tk.Entry(popup_conteudo, width=40)
+        entrada_titulo.pack(pady=5)
+
+        tk.Label(popup_conteudo, text="Descrição da Tarefa:", **ESTILO_TEXTO).pack(pady=5)
+        entrada_tarefa = tk.Text(popup_conteudo, height=5, width=40)
+        entrada_tarefa.pack(pady=5)
+    
+        opcoes = ["RECEPÇÃO", "ADMINISTRADOR", "FINANCEIRO", "FATURAMENTO"]
+        escolha_opcoes = tk.StringVar(value=opcoes[0])
+        tk.OptionMenu(popup_conteudo, escolha_opcoes, *opcoes).pack(pady=10)
+
+    def remover_tarefa(self):
+        try:
+            index_selecionado = self.lista_tarefas.curselection()[0]
+            id_tarefa = self.lista_tarefas.get(index_selecionado).split(" - ")[0].strip("● ").strip()
+            FB.remover_tarefa_bd(id_tarefa)
+            self.carregar_lista_tarefas()
+        except IndexError:
+            messagebox.showwarning("Aviso", "Selecione uma tarefa para remover.")
+
+    def marcar_concluida(self):
+        try:
+            index_selecionado = self.lista_tarefas.curselection()[0]
+            id_tarefa = self.lista_tarefas.get(index_selecionado).split(" - ")[0]
+
+            def salvar_descricao():
+                descricao = entrada_descricao.get("1.0", tk.END).strip()
+                if descricao:
+                    FB.atualizar_status_tarefa(id_tarefa, "Concluída", descricao, self.usuario)
+                    self.carregar_lista_tarefas()
+                    popup.destroy()
+                else:
+                    messagebox.showwarning("Aviso", "A descrição não pode estar vazia.")
+
+            popup, popup_conteudo = self.criar_popup("Descrição da Conclusão", salvar_descricao)
+            tk.Label(popup_conteudo, text="Descreva como concluiu a tarefa:", **ESTILO_TEXTO).pack(pady=5)
+            entrada_descricao = tk.Text(popup_conteudo, height=5, width=40)
+            entrada_descricao.pack(pady=10)
+        except IndexError:
+            messagebox.showwarning("Aviso", "Selecione uma tarefa para marcar como concluída.")
+
+if __name__ == "__main__":
+    App()
--- FuncoesBanco.py
+++ FuncoesBanco.py
@@ -11,21 +11,40 @@ def verificar_credenciais(usuario, senha):
 
     return resultado["ADMINISTRADOR"] if resultado else None
 
-def carregar_tarefas():
+def verificar_setor(usuario, senha):
     conexao = conexao_db()
     with conexao.cursor() as cursor:
+        cursor.execute("""SELECT SETOR FROM usuarios WHERE NOME = %s AND SENHA = %s""", (usuario,senha))
+        resultado = cursor.fetchone()
+    conexao.close()
+    return resultado["SETOR"] if resultado else None
 
-        cursor.execute("SELECT * FROM tarefas")
-        tarefas = cursor.fetchall()
+def select_tarefas(id_tarefa):
+    conexao = conexao_db()
+    with conexao.cursor() as cursor:
+        cursor.execute("""SELECT * FROM tarefas WHERE TAREFAS_ID = %s""", (id_tarefa))
+        resultado = cursor.fetchone()
+    conexao.close()
+    return resultado if resultado else None
+
+def carregar_tarefas(nivel_acesso, setor_destino):
+    conexao = conexao_db()
+    with conexao.cursor() as cursor:
+        verificar_credenciais
+        if nivel_acesso == "0":
+            cursor.execute("SELECT * FROM tarefas where SETOR = %s", (setor_destino))
+            tarefas = cursor.fetchall()
+        else:
+            cursor.execute("SELECT * FROM tarefas")
+            tarefas = cursor.fetchall()
     conexao.close()
 
     return tarefas
 
-def adicionar_tarefa_db(descricao):
+def adicionar_tarefa_db(titulo, descricao, setor):
     conexao = conexao_db()
     with conexao.cursor() as cursor:
-
-        cursor.execute("INSERT INTO tarefas (DESCRICAO) VALUES (%s)", (descricao,))
+        cursor.execute("INSERT INTO tarefas (TITULO, DESCRICAO, SETOR) VALUES (%s, %s, %s)", (titulo, descricao, setor))
     conexao.commit()
     conexao.close()
 
@@ -36,11 +55,11 @@ def remover_tarefa_bd(id_tarefa):
     conexao.commit()
     conexao.close()
 
-def atualizar_status_tarefa(id_tarefa, status):
+def atualizar_status_tarefa(id_tarefa, status, descricao, usuario):
     conexao = conexao_db()
     with conexao.cursor() as cursor:
 
         cursor.execute("UPDATE tarefas SET STATUS = %s WHERE TAREFAS_ID = %s", (status, id_tarefa))
+        cursor.execute("UPDATE tarefas SET TAREFA_SOLUCAO = %s, DATA_CONCLUSAO = CURRENT_TIMESTAMP, FUNCIONARIO_SOLUCAO = %s  WHERE TAREFAS_ID = %s", (descricao, usuario, id_tarefa))
     conexao.commit()
     conexao.close()
-
