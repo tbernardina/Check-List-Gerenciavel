eb1d643068a21091b17ee7c3c7c051e410ddc2e7
Thiago Reis Dalla Bernardina <trdallabernardina@gmail.com>
2024-12-23 12:44:09 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Variaveis de configuração de conexão com o servidor
-server_ip = "192.168.1.47"
+server_ip = "192.168.1.44"
 user = "GerenciadorCheckList"
 password = "GerenciadorCheckList123"
 database = "projetopython"
@@ -15,6 +15,6 @@ ESTILOS = {
     "janela": {"bg": "#2A5D48"},
     "texto": {"font": ("Sans-Serif Bold", 12, "bold"), "bg": "#2A5D48", "fg": "#C34E17"},
     "titulo": {"font": ("Comic Sans MS", 18, "bold"), "bg": "#2A5D48", "fg": "#C34E17"},
-    "texto_login": {"font": ("Arial", 12)},
+    "texto_login": {"font": ("Arial", 12, "bold"), "fg": "#37515F"},
     "lista_tarefas": {"font": ("Arial", 12), "fg": "#37515F"},
 }
