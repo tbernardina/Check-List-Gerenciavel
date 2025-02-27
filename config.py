287291a081a7591bd9d44de213a8cff3a1777992
Thiago Reis Dalla Bernardina <trdallabernardina@gmail.com>
2025-02-27 07:44:55 -0300

    Atualizado ano da licença

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Thiago Reis Dalla Bernardina
+# Copyright 2025 Thiago Reis Dalla Bernardina
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
